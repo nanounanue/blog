46f3faa9cbe8b0ce0cec097f08a8436697df2f24
Adolfo De Unánue <nanounanue@gmail.com>
2014-04-15 18:35:54 -0500

    Modificaciones para deployar a producción

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,58 +1,83 @@
+# -*- coding: utf-8 -*-
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'blog'
+set :deploy_user, 'deploy'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+# Detalles del repo
+set :scm, :git
+set :repo_url, 'git@github.com:nanounanue/blog.git'
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
 
-# Default value for :scm is :git
-# set :scm, :git
+# Rbenv
+set :rbenv_type, :system
+set :rbenv_ruby, '2.0.0p247'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
-# Default value for :format is :pretty
-# set :format, :pretty
+# ¿Cuántas versiones anteriores?
+set :keep_releases, 5
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+# Archivos que queremos symlink
+set :linked_files, %w{config/database.yml}
 
-# Default value for :pty is false
-# set :pty, true
+# Directorios que queremos de symlink
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+# ¿Qué pruebas se deben de ejecutar?
+set :tests, []
 
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set(:config_files, %w(
+nginx.conf
+database.example.yml
+log_rotation
+monit
+unicorn.rb
+unicorn_init.sh
+))
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set(:executable_config_files, %w(
+unicorn_init.sh
+))
+
+
+set(:symlinks, [
+                {
+                  source: "nginx.conf",
+                  link: "/etc/nginx/sites-enabled/#{full_app_name}"
+                },
+                {
+                  source: "unicorn_init.sh",
+                  link: "/etc/init.d/unicorn_#{full_app_name}"
+                },
+                {
+                  source: "log_rotation",
+                  link: "/etc/logrotate.d/#{full_app_name}"
+                },
+                {
+                  source: "monit",
+                  link: "/etc/monit/conf.d/#{full_app_name}.conf"
+                }
+               ]
+)
+
 
 namespace :deploy do
+  before :deploy, "deploy:check_revision"
+  before :deploy, "deploy:run_tests"
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+  after ':deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
 
-  after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+  before 'deploy:setup_config', 'nginx:remove_default_vhost'
+
+  after 'deploy:setup_config', 'nginx:reload'
+
+  after 'deploy:setup_config', 'monit:restart'
+
+  after 'deploy:publishing', 'deploy:restart'
 
 end
