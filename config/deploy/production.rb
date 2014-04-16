08516e97f6d129cf879fb753d129d0e7fb3687c2
Adolfo De Unánue <nanounanue@gmail.com>
2014-04-15 19:44:14 -0500

    Modificaciones para que ejecute, ya solo falta configurar la base de datos y el unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :stage, :production
 set :branch,"master"
 
 set :server_name, "blog"
-set :full_app_name, "#{fetch(:application)_#{fetch(:stage)}"
+set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
 server '107.170.81.168', user: 'deploy', roles: %w{web app db}, primary: true
 
